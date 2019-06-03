f03cf6d42d4cfd309cd0b77bb529589106e14d1d
Charles Kubiak <crkubiak@Charless-MacBook-Pro.local>
2019-06-03 16:49:26 -0500

    skeleton for runner, board, player

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,6 +7,19 @@ public class TicTacToe {
     return "runner";
     }
 
+    public String createBoard() {
+        String[] boardChoices = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        String createBoard = String.format("%s|%s|%s\n-+-+-\n%s|%s|%s\n-+-+-\n%s|%s|%s", boardChoices[0], boardChoices[1], boardChoices[2], boardChoices[3], boardChoices[4], boardChoices[5], boardChoices[6], boardChoices[7], boardChoices[8]);
+        return createBoard;
+    }
+
+    public String createPlayer(String name) {
+        String pName = name;
+        return pName;
+    }
+
+
+
     public static void main(String[] args) {
         TicTacToe newGame = new TicTacToe();
 
--- test/TicTacToeTest.java
+++ test/TicTacToeTest.java
@@ -9,4 +9,16 @@ class TicTacToeTest {
         assertEquals("runner", testGame.runner());
     }
 
+    @Test
+    public void testCreateBoard() {
+        TicTacToe testGame = new TicTacToe();
+        assertEquals("1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9", testGame.createBoard());
+    }
+
+    @Test
+    public void testCreatePlayer() {
+        TicTacToe testGame = new TicTacToe();
+        assertEquals("Charles", testGame.createPlayer("Charles"));
+    }
+
 }
